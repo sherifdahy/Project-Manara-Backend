6a591566ba7307cbf9763d29cb983446e7cdd4e5
Sherif Dahy <shrifm2017@gmail.com>
2026-02-06 18:42:13 +0200

    add unary relation to role

---
+++ App.Infrastructure/Migrations/20260206163944_add_unary_relation_to_role.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_unary_relation_to_role : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RoleId",
+                table: "AspNetRoles",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 100,
+                column: "RoleId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 101,
+                column: "RoleId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 102,
+                column: "RoleId",
+                value: null);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoles_RoleId",
+                table: "AspNetRoles",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetRoles_AspNetRoles_RoleId",
+                table: "AspNetRoles",
+                column: "RoleId",
+                principalTable: "AspNetRoles",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetRoles_AspNetRoles_RoleId",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetRoles_RoleId",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "RoleId",
+                table: "AspNetRoles");
+        }
+    }
+}
--- App.Services/obj/Debug/net10.0/App.Services.AssemblyInfo.cs
+++ App.Services/obj/Debug/net10.0/App.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0dd4bccfaadb875c434312040839e52157006e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df6309b0d278f9b5795f2043fa76257f17717251")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
