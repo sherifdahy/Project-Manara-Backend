f3970f33a7fb6636047e893326ad20ae960079a6
OmarZaky <omarzaky078@gmail.com>
2026-02-10 18:29:27 +0200

    add the filters tags

--- App.Application/obj/Debug/net10.0/App.Application.AssemblyInfo.cs
+++ App.Application/obj/Debug/net10.0/App.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca791d43b67beb130013af11de6fbd1998a11933")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af6b35864d98d677677bd4ddc2f33ef2c424422a")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- App.Infrastructure/obj/Debug/net10.0/App.Infrastructure.AssemblyInfo.cs
+++ App.Infrastructure/obj/Debug/net10.0/App.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca791d43b67beb130013af11de6fbd1998a11933")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af6b35864d98d677677bd4ddc2f33ef2c424422a")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
