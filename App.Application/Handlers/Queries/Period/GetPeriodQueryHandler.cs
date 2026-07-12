964d78166dd2dae51732e95174e2c61ca7e1b00b
Sherif Dahy <shrifm2017@gmail.com>
2026-07-12 16:51:36 +0300

    update

--- App.Application/Handlers/Queries/Period/GetPeriodQueryHandler.cs
+++ App.Application/Handlers/Queries/Period/GetPeriodQueryHandler.cs
@@ -10,7 +10,7 @@ public class GetPeriodQueryHandler(IUnitOfWork unitOfWork,PeriodErrors periodErr
 
     public async Task<Result<PeriodResponse>> Handle(GetPeriodQuery request, CancellationToken cancellationToken)
     {
-        var period = _unitOfWork.Periods.FindAsync(x => x.Id == request.Id);
+        var period = await _unitOfWork.Periods.FindAsync(x => x.Id == request.Id);
 
         if (period is null)
             return Result.Failure<PeriodResponse>(_periodErrors.NotFound);
